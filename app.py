fb4d5c4eaf1de0daeda86d2bc114dfc793c2eb70
Lucifurge <tasogaresaya@gmail.com>
2025-01-04 10:37:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -138,4 +138,4 @@ def submit():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':  
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)), debug=False)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)), debug=True)
